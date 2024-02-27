2f3424827755c23e5485314a87c264f0dbff59f5
adityasxna <adityasaxena0022@gmail.com>
2024-02-27 15:54:52 +0530

    bkc

---
+++ bkc.java
@@ -0,0 +1,11 @@
+public class bkc{
+    public static void main(String[] args) {
+        int temp1 = 1;
+        
+        for(int i = 0; i<5; i++) {
+            temp1 = temp1 + 1;
+        }
+        System.out.println("A"+(temp1+10));
+    }
+ 
+}
